17f8acd795334083755c9cf25a0fdb81db14fec2
Sahana Basapathi <srsahana67@gmail.com>
2022-11-10 15:23:58 -0500

    Category prediction dataset

--- scripts/extract_tweets.py
+++ scripts/extract_tweets.py
@@ -1,54 +1,51 @@
 import tweepy,json
-access_token = "1485395819700404229-NZEkwdqLsfUXlAXxgSS18o0Ne3ZtIv"
-access_token_secret = "l1TQjKDz6fmQS8tQ2985vYvimYIEme3l6XZx8zFU8LSIT"
-consumer_key = "J6t9sNlYpl4jf7UBwpQlYPqcE"
-consumer_secret = "k5OmdaoEMwX41WPuoYnooTZ7yOYzHAnHXyXhpeecYKuRd6eZ0E"
+import pandas as pd
+import sys
+def get_tweets(hashtags, tweet_text):
 
-auth= tweepy.OAuthHandler(consumer_key,consumer_secret)
-auth.set_access_token(access_token,access_token_secret)
+    print(hashtags)
+    # search_results = api.search_tweets(q=ha  # shtags, count=200)
+    tweets = tweepy.Cursor(api.search,
+                                q = hashtags, lang = "en",
+                               # 200 is the max imum allowed count
+                               # count=500,
+                               include_rts = False,
+                               # Necessary to keep full_text
+                               # otherwise only the first 140 words are extracted
+                              tweet_mode = "extended"
+                               ).items(1000)
+    # print(tweet.id_str, tweet.created_at, tweet.favorite_count, tweet.retweet_count, tweet.full_text.encode("utf-8").decode("utf-8"))
+    col_tweets = [[tweet.created_at, tweet.full_text.encode("utf-8").decode("utf-8"),tweet.entities['hashtags']] for tweet in tweets]
+    # print(col_tweets)
+    df = pd.DataFrame(col_tweets, columns=["created_at", "tweet_text", "hashtags"])
+    tweet_text_ = pd.concat([tweet_text, df], ignore_index=True)
 
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
-tweet_list=[]
+    #save to csv file
+    tweet_text_.to_csv("Tweets.csv", index=False)
+    print("Total tweets: ", tweet_text_.shape)
+    print("Collected tweets!!")
 
-# class MyStreamListener(tweepy.Stream):  tweepy.StreamListener
-class MyStreamListener(tweepy.StreamListener):
-    def __init__(self,api=None):
-        super(MyStreamListener,self).__init__()
-        self.num_tweets=0
-        self.file=open("tweet.txt","w+")
-    def on_status(self,status):
-        tweet=status.text
-        print(tweet)
-        self.file.write(json.dumps(tweet)+ '\n')
-        tweet_list.append(status)
-        self.num_tweets+=1
-        if self.num_tweets<2:
-            return True
-        else:
-            return False
-        self.file.close()
+if __name__ == '__main__':
+    access_token = "1485395819700404229-NZEkwdqLsfUXlAXxgSS18o0Ne3ZtIv"
+    access_token_secret = "l1TQjKDz6fmQS8tQ2985vYvimYIEme3l6XZx8zFU8LSIT"
+    consumer_key = "J6t9sNlYpl4jf7UBwpQlYPqcE"
+    consumer_secret = "k5OmdaoEMwX41WPuoYnooTZ7yOYzHAnHXyXhpeecYKuRd6eZ0E"
 
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
 
-#create streaming object and authenticate
-# l = MyStreamListener()
-# stream = tweepy.Stream(auth, MyStreamListener())
-# l = MyStreamListener(stream)
-# auth,MyStreamListener()
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
 
-myStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
-#this line filters twiiter streams to capture data by keywords
-myStream.filter(track=['depression', 'sadness', 'fatigue', 'loneliness', 'loss of interest',
-'low energy', 'low concentration'])
+    try:
+        tweet_text = pd.read_csv("Tweets.csv")
+        # tweet_text = pd.DataFrame(columns=["created_at", "tweet_text", "hashtags"])
+
+    except:
+        tweet_text = pd.DataFrame(columns=["created_at", "tweet_text", "hashtags"])
 
 
-# tweets_data_path='tweet.txt'
-# tweets_data=[]
-# tweets_file=open(tweets_data_path,"r")
-# #read in tweets and store on list
-# for line in tweets_file:
-#     tweet=json.loads(line)
-#     tweets_data.append(tweet)
-
-# tweets_file.close()
-# print(tweets_data[0])
+    #need to take hashtags as arguments
+    hashtags = sys.argv[1:]
+    print(hashtags)
+    for h in hashtags:
+        get_tweets("#"+h, tweet_text)
